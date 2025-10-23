3e99525f7d273951153528d4419811fe5f4aaaaf
Vetri-78640 <vetriselven5@gmail.com>
2025-10-23 20:30:06 +0530

    chore: ignore node_modules and build files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -68,38 +68,38 @@ function Home() {
             ))}
           </div>
         )}
-       <div
-  style={{
-    textAlign: 'center',
-    padding: '1.5rem 0',
-    marginTop: '4rem',
-    backdropFilter: 'blur(8px)',
-    color: '#333',
-    fontWeight: '500',
-    fontSize: '1rem',
-    borderTop: '1px solid rgba(0,0,0,0.05)',
-  }}
->
-  <h1 style={{ marginBottom: '0.5rem', color: '#b5a7f7'}}>Designed & Developed by Vetri</h1>
-  <div style={{ display: 'flex', justifyContent: 'center', gap: '1.5rem', fontSize: '1.4rem' }}>
-    <a
-      href="https://www.linkedin.com/in/vetriselvan-r-a238b7263"
-      target="_blank"
-      rel="noopener noreferrer"
-      style={{ color: '#0077b5', textDecoration: 'none' }}
-    >
-      LinkedIn
-    </a>
-    <a
-      href="https://www.instagram.com/im.vetri"
-      target="_blank"
-      rel="noopener noreferrer"
-      style={{ color: '#E1306C', textDecoration: 'none' }}
-    >
-      Instagram
-    </a>
-  </div>
-</div>
+        <div
+          style={{
+            textAlign: 'center',
+            padding: '1.5rem 0',
+            marginTop: '4rem',
+            backdropFilter: 'blur(8px)',
+            color: '#333',
+            fontWeight: '500',
+            fontSize: '1rem',
+            borderTop: '1px solid rgba(0,0,0,0.05)',
+          }}
+        >
+          <h1 style={{ marginBottom: '0.5rem', color: '#b5a7f7'}}>Designed & Developed by Vetri</h1>
+          <div style={{ display: 'flex', justifyContent: 'center', gap: '1.5rem', fontSize: '1.4rem' }}>
+            <a
+              href="https://www.linkedin.com/in/vetriselvan-r-a238b7263"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: '#0077b5', textDecoration: 'none' }}
+            >
+              LinkedIn
+            </a>
+            <a
+              href="https://www.instagram.com/im.vetri"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: '#E1306C', textDecoration: 'none' }}
+            >
+              Instagram
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
